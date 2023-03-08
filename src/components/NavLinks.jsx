f3da0f6819a1246d1bdcc409122cbb2f8969c3d3
Luis Rosal <luiss.rosalg.13@gmail.com>
2023-03-08 15:41:23 -0400

    removed pricing from the navlinks

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -8,7 +8,6 @@ export function NavLinks() {
   return [
     ['Home', '#home'],
     ['Contact', '#contact'],
-    ['Pricing', '#pricing'],
     ['FAQs', '#faqs'],
   ].map(([label, href], index) => (
     <Link
